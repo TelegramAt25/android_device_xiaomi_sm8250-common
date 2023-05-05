050d1b05047805741501fb4ded508261580b2c67
Reinazhard <reinazhard@gmail.com>
2023-05-05 04:36:05 +0200

    sm8250-common: Import Android Go performance tuning
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kona.mk
+++ kona.mk
@@ -163,6 +163,14 @@ endif
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
