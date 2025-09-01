1a25d7644c7c930851a04fdf4a065f0a317c231b
fukiame <fukiame@proton.me>
2025-09-01 14:57:19 +0700

    Revert "sm8250-common: Add libwfdservice_shim for WFD"
    This reverts commit 9ff4ef7dafcbf4ff06f61a2c0343618a058005b3.

--- extract-files.py
+++ extract-files.py
@@ -18,8 +18,6 @@ from extract_utils.fixups_lib import (
 )
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/bin/wfdservice64': blob_fixup()
-        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
