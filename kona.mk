ddfbf1519900e23a8cba9592f3e734440b7b6317
fukiame <fukiame@proton.me>
2025-11-25 00:49:50 +0700

    sm8250-common: drop parts
    Signed-off-by: fukiame <fukiame@proton.me>

--- kona.mk
+++ kona.mk
@@ -159,10 +159,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 endif
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Do not generate libartd.
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
